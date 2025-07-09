40f51e55ee942fd704b75c0fd5bd1da11a3ddf31
xingxq <xingxq@yeah.net>
2025-07-09 16:32:31 +0800

    修改API服务器

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://117.72.122.86:21114".to_owned()
 }
 
 #[inline]
